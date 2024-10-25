868b23a0ab662452173cfca149c1cfe9c8c2fb49
Shivani <76843394+Shivani2501@users.noreply.github.com>
2024-10-25 09:04:50 +0530

    Update Q15.c

--- Handsonlist2/Q15.c
+++ Handsonlist2/Q15.c
@@ -1,3 +1,12 @@
+/*
+============================================================================
+Name : 15.c
+Author : Shivani Shukla
+Description : Write a simple program to create a pipe, write to the pipe, read from pipe and display on
+the monitor.
+Date: 20th Sep, 2024.
+============================================================================
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
