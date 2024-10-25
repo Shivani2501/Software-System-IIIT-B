fb61491eff12e8882db0523c033be29e7c9d35bd
Shivani <76843394+Shivani2501@users.noreply.github.com>
2024-10-25 08:43:08 +0530

    Update Q8a.c

--- Handsonlist2/Q8a.c
+++ Handsonlist2/Q8a.c
@@ -1,3 +1,17 @@
+/*
+============================================================================
+Name : 8.c
+Author : Shivani Shukla
+Description : Write a separate program using signal system call to catch the following signals.
+a. SIGSEGV
+b. SIGINT
+c. SIGFPE
+d. SIGALRM (use alarm system call)
+e. SIGALRM (use setitimer system call)
+f. SIGVTALRM (use setitimer system call)
+g. SIGPROF (use setitimer system call)
+============================================================================
+*/
 #include <stdio.h>
 #include<stdlib.h>
 #include<signal.h>
