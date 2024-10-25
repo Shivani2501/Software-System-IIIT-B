3810661ae86b619b800fa32fce906457060b36f0
Shivani <76843394+Shivani2501@users.noreply.github.com>
2024-10-25 09:01:22 +0530

    Update Q13a.c

--- Handsonlist2/Q13a.c
+++ Handsonlist2/Q13a.c
@@ -1,3 +1,13 @@
+/*
+============================================================================
+Name : 13.c
+Author : Shivani Shukla
+Description :  Write two programs: first program is waiting to catch SIGSTOP signal, the second program
+will send the signal (using kill system call). Find out whether the first program is able to catch
+the signal or not.
+Date: 20th Sep, 2024.
+============================================================================
+*/
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
