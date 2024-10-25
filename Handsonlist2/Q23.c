0430fe0779d4e3d509d1b24bab6517435f89ff46
Shivani <76843394+Shivani2501@users.noreply.github.com>
2024-10-25 07:55:25 +0530

    Update Q23.c

--- Handsonlist2/Q23.c
+++ Handsonlist2/Q23.c
@@ -1,3 +1,10 @@
+/*
+Name : 23.c
+Author : Shivani Shukla
+Description : Write a program to print the maximum number of files can be opened within a process and
+size of a pipe (circular buffer).
+Date: 21th Sep, 2024.
+    */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/resource.h>
