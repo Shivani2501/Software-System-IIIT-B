b9c1034045329d0489f9be289ac4e3f86c76a61f
Shivani <76843394+Shivani2501@users.noreply.github.com>
2024-10-25 07:28:49 +0530

    Update Q25.c

--- Handsonlist2/Q25.c
+++ Handsonlist2/Q25.c
@@ -1,3 +1,17 @@
+/*Name : 26.c
+Author : Shivani Shukla
+Description :  Write a program to print a message queue's (use msqid_ds and ipc_perm structures)
+a. access permission
+b. uid, gid
+c. time of last message sent and received
+d. time of last change in the message queue
+d. size of the queue
+f. number of messages in the queue
+g. maximum number of bytes allowed
+h. pid of the msgsnd and msgrcv
+
+Date: 21th Sep, 2024.
+    */
 #include <sys/msg.h>
 #include <stdio.h>
 #include <sys/ipc.h>
